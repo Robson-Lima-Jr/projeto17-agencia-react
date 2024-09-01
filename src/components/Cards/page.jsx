0b2515e8c3ac817289fa720d6cb47a01fe5452a2
Robson-Jr <robsonjuniorsn@gmail.com>
2024-09-01 13:51:40 -0300

    componentes experiencia e cards, concluidos

--- src/components/Cards/page.jsx
+++ src/components/Cards/page.jsx
@@ -1,13 +1,13 @@
 import styles from "./Cards.module.css";
 
-export default function Cards({data, titulo, empresa, texto}) {
+export default function Cards({darkMode,data, titulo, empresa, texto}) {
     return(
-        <div className={styles.cards_area}>
-            <span>{data}</span>
-
-            <h4>{titulo}</h4>
-
-            <small>{empresa}</small>
+        <div className={darkMode ? styles.cards_darkMode : styles.cards_lightMode}>
+            <div className={styles.cards_titulos}>
+                <span>{data}</span>
+                <h4>{titulo}</h4>
+                <small>{empresa}</small>
+            </div>
 
             <p>{texto}</p>
         </div>
--- src/components/SecaoExperiencia/page.jsx
+++ src/components/SecaoExperiencia/page.jsx
@@ -14,21 +14,24 @@ export default function SecaoExperiencia(props) {
 
             <div className={styles.container_cards}>
                 <Cards
+                    darkMode={props.darkMode}
                     data={"JUNHO 2012 - 2016"}
                     titulo={"Web Designer"}
                     empresa={"Pied Piper StartUp."}
                     texto={"Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"} />
                 <Cards
-                    data={"JUNHO 2012 - 2016"}
-                    titulo={"Web Designer"}
-                    empresa={"Pied Piper StartUp."}
-                    texto={"Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"} />
+                    darkMode={props.darkMode}
+                    data={"AGOSTO 2016 - 2019"}
+                    titulo={"Product Designer"}
+                    empresa={"E Corp."}
+                    texto={"Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra"} />
 
                 <Cards
-                    data={"JUNHO 2012 - 2016"}
-                    titulo={"Web Designer"}
-                    empresa={"Pied Piper StartUp."}
-                    texto={"Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"} />
+                    darkMode={props.darkMode}
+                    data={"FEVEREIRO 2019 - 2021"}
+                    titulo={"Digital Consulting"}
+                    empresa={"Arasaka Inc."}
+                    texto={"Consultoria em estratégias digitais para todas as etapas do ciclo do projeto, desde a definição inicial até a execução"} />
 
             </div>
         </section>
